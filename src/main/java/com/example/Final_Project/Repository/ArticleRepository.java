6cfd54fd6399cf40670b99e4b23f53ed8a4594ac
Eddy <marill94727@naver.com>
2023-09-10 23:26:00 +0900

    React 연동을 위한 CORS 설정 및 엔드포인트 경로 수정

--- src/main/java/com/example/Final_Project/Repository/ArticleRepository.java
+++ src/main/java/com/example/Final_Project/Repository/ArticleRepository.java
@@ -11,17 +11,17 @@ public interface ArticleRepository extends JpaRepository<Article, Long> {
 
     // 여러 검색 조건을 지원하기위해 @Query 어노테이션을 사용하여 사용자 정의 쿼리 작성
     @Query("SELECT a FROM Article a WHERE " +
-            "(:search IS NULL OR " +
-            "a.abstract_en LIKE %:search% OR " +
-            "a.abstract_ko LIKE %:search% OR " +
-            "a.author_name LIKE %:search% OR " +
-            "a.institution LIKE %:search% OR " +
-            "a.journal_name LIKE %:search% OR " +
-            "a.keyword LIKE %:search% OR " +
-            "a.major LIKE %:search% OR " +
-            "a.title_en LIKE %:search% OR " +
-            "a.title_ko LIKE %:search%)")
+            "(:searchkeyword IS NULL OR " +
+            "a.abstract_en LIKE %:searchkeyword% OR " +
+            "a.abstract_ko LIKE %:searchkeyword% OR " +
+            "a.author_name LIKE %:searchkeyword% OR " +
+            "a.institution LIKE %:searchkeyword% OR " +
+            "a.journal_name LIKE %:searchkeyword% OR " +
+            "a.keyword LIKE %:searchkeyword% OR " +
+            "a.major LIKE %:searchkeyword% OR " +
+            "a.title_en LIKE %:searchkeyword% OR " +
+            "a.title_ko LIKE %:searchkeyword%)")
 
     // "findByKeyword" 메서드는 JPA 쿼리 메서드로, 데이터베이스에서 키워드를 포함하는 항목을 대소문자 구분 없이 검색하기 위해 사용
-    List<Article> findByKeyword(String search);
+    List<Article> findByKeyword(String searchkeyword);
 }
