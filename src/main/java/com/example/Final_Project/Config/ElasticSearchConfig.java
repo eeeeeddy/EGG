f43226a719f9e36ed6bfc1bff2ecac42e2b3f3de
eeeeeddy <marill94727@naver.com>
2023-10-10 10:49:13 +0900

    ElasticSearch 연동

--- src/main/java/com/example/Final_Project/Config/ElasticSearchConfig.java
+++ src/main/java/com/example/Final_Project/Config/ElasticSearchConfig.java
@@ -1,21 +1,11 @@
 package com.example.Final_Project.Config;
 
-import com.example.Final_Project.Entity.ElasticSearch;
 import org.apache.http.HttpHost;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate;
-import org.springframework.data.elasticsearch.core.convert.ElasticsearchConverter;
-import org.springframework.data.elasticsearch.core.convert.MappingElasticsearchConverter;
-import org.springframework.data.elasticsearch.core.mapping.SimpleElasticsearchMappingContext;
-import org.springframework.data.elasticsearch.core.mapping.SimpleElasticsearchPersistentEntity;
-import org.springframework.data.elasticsearch.core.mapping.SimpleElasticsearchPersistentProperty;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
-
-import java.util.Collections;
 
 @Configuration
 @EnableElasticsearchRepositories(basePackages = "com.example.Final_Project.Repository") // Elasticsearch repository 패키지 설정
